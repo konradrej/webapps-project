3c2223909c59e553cdc942ec1367cfb31479bdda
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2022-02-10 15:45:26 +0100

    Add cors to server and create react based client project.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import Express from "express";
+import cors from "cors";
 import { userRouter } from "./router/user.router";
 import { postRouter } from "./router/post.router";
 
 export const app : Express.Express = Express();
 
 app.use(Express.json());
+app.use(cors());
 app.use("/user", userRouter());
 app.use("/post", postRouter());
