72717b68f2317ad08cace7e7f7d1d29a4c57f6cb
applephong <apple_phong@hotmail.com>
2022-02-12 15:53:03 +0100

    onChange on input

--- client/src/Components/SignIn.tsx
+++ client/src/Components/SignIn.tsx
@@ -10,9 +10,22 @@ export default class SignInPopUp extends React.Component<{}>{
   private userNamePlaceholder : string ="Enter username";
   private passwordPlaceholder : string ="Enter password";
 
+  state= {
+    inputUsername: "",
+    inputPassword: ""
+  }
+
+  onChangeUserName = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({ inputUsername: e.currentTarget.value });
+  };
+
+  onChangePassword = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({ inputPassword: e.currentTarget.value });
+  };
+
 
   onSignInHandler = () => {
-    console.log("hej")
+
   }
 
   onRegisterHandler = () => {
@@ -26,11 +39,11 @@ export default class SignInPopUp extends React.Component<{}>{
           <div className="input-content">  
             <div className="input-sub-content">
               <h2><b>{ this.userNameText }</b></h2>
-              <input type="text" placeholder={ this.userNamePlaceholder } />
+              <input type="text" value={ this.state.inputUsername } placeholder={ this.userNamePlaceholder } onChange={ this.onChangeUserName } />
             </div>
             <div className="input-sub-content">
               <h2><b>{ this.userPasswordText }</b></h2>
-              <input type="password" placeholder={ this.passwordPlaceholder }/>
+              <input type="password" value={ this.state.inputPassword } placeholder={ this.passwordPlaceholder } onChange={ this.onChangePassword }/>
             </div>
             <div className="pop-up-button-container">
               <button className="pop-up-button" onClick={ this.onRegisterHandler }>
--- client/src/Components/SignUp.tsx
+++ client/src/Components/SignUp.tsx
@@ -12,6 +12,24 @@ export default class SignUpPopUp extends React.Component<{}>{
   private passwordPlaceholder : string ="Enter password";
   private emailPlaceholder : string = "Enter e-mail";
 
+  state = {
+    inputUserName: "",
+    inputPassword: "",
+    inputEmail: "",
+  }
+
+  onChangeUserName = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({ inputUsername: e.currentTarget.value });
+  };
+
+  onChangePassword = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({ inputPassword: e.currentTarget.value });
+  };
+
+  onChangeEmail = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({ inputEmail: e.currentTarget.value });
+  };
+
   onGoBackHandler = () => {
     console.log("hej3")
   }
@@ -27,15 +45,15 @@ export default class SignUpPopUp extends React.Component<{}>{
           <div className="input-content">  
             <div className="input-sub-content">
               <h2><b>{ this.userNameText }</b></h2>
-              <input type="text" placeholder={ this.userNamePlaceholder } />
+              <input type="text" value={ this.state.inputUserName } placeholder={ this.userNamePlaceholder } onChange={ this.onChangeUserName }  />
             </div>
             <div className="input-sub-content">
               <h2><b>{ this.userPasswordText }</b></h2>
-              <input type="password" placeholder={ this.passwordPlaceholder }/>
+              <input type="password" value={ this.state.inputPassword } placeholder={ this.passwordPlaceholder } onChange={ this.onChangePassword }/>
             </div>
             <div className="input-sub-content">
               <h2><b>{ this.userEmailText }</b></h2>
-              <input type="text" placeholder={ this.emailPlaceholder }/>
+              <input type="text" value={ this.state.inputEmail } placeholder={ this.emailPlaceholder } onChange={ this.onChangeEmail }/>
             </div>
             <div className="pop-up-button-container">
               <button className="pop-up-button" onClick={ this.onGoBackHandler }>
