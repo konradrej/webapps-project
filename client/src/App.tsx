789159c1817108efa602a1fc61f23dc1f3c52822
Erik Kieu <erik.kieu@gmail.com>
2022-02-12 13:26:14 +0100

    Header, bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
