72a2a2b0e761b6358248580439a3aa1ed2ee10d1
applephong <apple_phong@hotmail.com>
2022-02-13 14:27:03 +0100

    apply bootstrap

--- client/src/Components/CardDetails.tsx
+++ client/src/Components/CardDetails.tsx
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 import PopUp from './Pop-up';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default class CardDetailsPopUp extends React.Component<{ postImageURL: string , postTitle: string, userImage : string, userName : string, postDate : string, postDescription : string}>{
+type Props = {
+  postImageURL: string,
+  postTitle: string,
+  userImage : string,
+  userName : string, 
+  postDate : string,
+  postDescription : string
+}
+
+export default class CardDetailsPopUp extends React.Component<Props>{
 
   private descriptionText: string = "Description: "
   private createdAtText: string = "Created at: "
 
   // TODO onclick userProfile
 
-
   render(){
     return(
       <PopUp>
-        <div className="post-pop-up">
-          <div className="post-content">
-            <img className="post-image"src= {this.props.postImageURL} alt="alt"/>
-            <div className="post-info">
-              <h2>{ this.props.postTitle }</h2>
+        <div className="post-content">
+          <Container fluid="md">
+            <Row>
+              <Col md={8}>
+              <img className="post-image"src= {this.props.postImageURL} alt="Image not rendered"/>
+              </Col>
+              <Col md={4} className='post-info'>
+              <h4>{ this.props.postTitle }</h4>
               <div className="post-user-info">
                 <img className ="post-user-profile" src={this.props.userImage} alt="profile" />
-                <h4 className="username"><b>{ this.props.userName} </b></h4>
+                <h6 className="username"><b>{ this.props.userName} </b></h6>
               </div>
-              <h4>{ this.createdAtText } </h4>
+              <h5>{ this.createdAtText } </h5>
               <p>{ this.props.postDate }</p>
-              <h4>{ this.descriptionText }</h4>
+              <h5>{ this.descriptionText }</h5>
               <p>{ this.props.postDescription }</p>
-            </div>
-          </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </PopUp>
     )
