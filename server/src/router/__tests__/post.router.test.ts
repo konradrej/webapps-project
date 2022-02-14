113c9821dd71de1a85222f21d78ac84c590c657d
Erik Kieu <erik.kieu@gmail.com>
2022-02-14 12:59:18 +0100

    Post  router and tests

---
+++ server/src/router/__tests__/post.router.test.ts
@@ -0,0 +1,112 @@
+import Express from "express";
+import SuperTest from "supertest";
+
+import { makePostRouter, postRouter } from "../post.router";
+import { IPostService, } from "../../service/post.service";
+import { Post } from "../../model/post.interface";
+import { User } from "../../model/user.interface";
+
+// Create the creator of the post
+const creator: User =
+{
+    id: 1,
+    username: "creatorName",
+    password: "creatorPassword",
+    email: "creatorMail",
+    profileImageUrl: "creatorProfile",
+    description: "creatorDescription",
+    posts: [],
+    createdAt: new Date(),
+};
+const updatedCreator: User =
+{
+    id: 2,
+    username: "updatedCreatorName",
+    password: "updatedCreatorPassword",
+    email: "updatedCreatorMail",
+    profileImageUrl: "updatedCreatorProfile",
+    description: "updatedCreatorDescription",
+    posts: [],
+    createdAt: new Date(),
+};
+
+
+test("A PUT request to /createPost should send a response of post successfully created", () => {
+    class MockPostService implements IPostService {
+        createPost = async (title: string, description: string, imageUrl: string, creator: User): Promise<Post> => {
+            return <Post>{};
+        }
+
+        getPosts = async (order: string): Promise<Array<Post>> => {
+            throw ("Wrong method called");
+        }
+
+        updatePost = async (id: number, newTitle: string, newDescription: string, verifyCreator: User): Promise<boolean> => {
+            throw ("Wrong method called");
+        }
+
+        getPost = async (id: number): Promise<Post> => {
+            throw ("Wrong method called");
+        }
+
+        findById = async (id: number): Promise<Post | null> => {
+            throw ("Wrong method called");
+        }
+    }
+
+    const postService: MockPostService = new MockPostService();
+
+    const router: Express.Express = Express();
+
+    router.use(Express.json());
+
+    router.use(makePostRouter(postService));
+
+    const request: SuperTest.SuperTest<SuperTest.Test> = SuperTest(router);
+
+    return request.post("/createPost").send({ title: "titleTest", description: "desscriptionTest", imageUrl: "imageTest", creator: creator }).then((res) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ status: "post successfully created" });
+    })
+})
+
+test("A POST request to /updatePost should send a response of post sucessfully updated", () => {
+    class MockPostService implements IPostService {
+        updatePost = async (id: number, newTitle: string, newDescription: string, verifyCreator: User): Promise<boolean> => {
+            return true;
+        }
+
+        createPost = async (title: string, description: string, imageUrl: string, creator: User): Promise<Post> => {
+            throw ("Wrong method called")
+        }
+
+        getPosts = async (order: string): Promise<Array<Post>> => {
+            throw ("Wrong method called");
+        }
+
+        getPost = async (id: number): Promise<Post> => {
+            throw ("Wrong method called");
+        }
+
+        findById = async (id: number): Promise<Post | null> => {
+            throw ("Wrong method called");
+        }
+    }
+
+    const postService: MockPostService = new MockPostService();
+
+    const router: Express.Express = Express();
+
+    router.use(Express.json());
+
+    router.use(makePostRouter(postService));
+
+    const request: SuperTest.SuperTest<SuperTest.Test> = SuperTest(router);
+
+    return request.post("/updatePost").send({ title: "updateTitleTest", description: "updateDesscriptionTest", imageUrl: "updateImageTest", creator: updatedCreator }).then((res) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ status: "post successfully updated" });
+    })
+
+
+})
--- server/src/service/post.service.ts
+++ server/src/service/post.service.ts
@@ -1,19 +1,21 @@
 import { Post } from "../model/post.interface";
 import { User } from "../model/user.interface";
 
-export interface IPostService  {
-  getPosts(order : string) : Promise<Array<Post>>
-  createPost(title : string, description : string, imageUrl : string, creator : User ) : Promise<Post>
-  updatePost(id : number, newTitle : string, newDescription : string, verifyCreator : User) : Promise<boolean>
-  getPost : (id: number) => Promise<Post>
+export interface IPostService {
+  getPosts(order: string): Promise<Array<Post>>
+  createPost(title: string, description: string, imageUrl: string, creator: User): Promise<Post>
+  updatePost(id: number, newTitle: string, newDescription: string, verifyCreator: User): Promise<boolean>
+  getPost: (id: number) => Promise<Post>
+  findById(id: number): Promise<Post | null>
+
   /*deletePost(id : number, verifyCreator : User) : Promise<boolean>*/
 }
 
-export class PostService implements IPostService{
-  private posts : { [key : number] : Post };
-  private postIdCounter : number = 0;
+export class PostService implements IPostService {
+  private posts: { [key: number]: Post };
+  private postIdCounter: number = 0;
 
-  constructor(posts : { [key : number] : Post }){
+  constructor(posts: { [key: number]: Post }) {
     this.posts = posts;
     // Order posts and get the highest id first and begin with that id as a counter
     this.postIdCounter = !posts ? Object.values(this.posts).sort((a, b) => a.id < b.id ? 1 : -1)[0].id : 0;
@@ -24,19 +26,19 @@ export class PostService implements IPostService{
     return this.posts[id] ?? null;
   }
   // Return all posts given order
-  getPosts : (order : string) => Promise<Post[]> =
-    async (order : string) => {
-      switch(order){
+  getPosts: (order: string) => Promise<Post[]> =
+    async (order: string) => {
+      switch (order) {
         // A-Z
         case "Alphabetic": {
           return Object.values(this.posts).sort(
-            (a, b) => a.title < b.title ? -1 : 1 
+            (a, b) => a.title < b.title ? -1 : 1
           )
         }
         // Z-A
         case "Reverse": {
           return Object.values(this.posts).sort(
-            (a, b) => a.title < b.title ? 1 : -1 
+            (a, b) => a.title < b.title ? 1 : -1
           )
         }
         // Recency
@@ -49,20 +51,20 @@ export class PostService implements IPostService{
     }
 
   // Returns true if new post is created, invalid if title, imageURL, creator is undefined/null
-  createPost : (title: string, description: string, imageUrl: string, creator: User) => Promise<Post>  = 
+  createPost: (title: string, description: string, imageUrl: string, creator: User) => Promise<Post> =
     async (title: string, description: string, imageUrl: string, creator: User) => {
-      if(!title){
+      if (!title) {
         throw Error("Missing title");
       }
-      if(!imageUrl){
+      if (!imageUrl) {
         throw Error("Missing image URL");
       }
-      if(!creator){
+      if (!creator) {
         throw Error("Must be signed in");
       }
 
       this.postIdCounter++;
-      const newPost : Post = {
+      const newPost: Post = {
         id: this.postIdCounter,
         title: title,
         description: description,
@@ -77,27 +79,27 @@ export class PostService implements IPostService{
     }
 
   // Returns true if post is updated given id and user id
-  updatePost : (id: number, newTitle: string, newDescription: string, verifyCreator: User) => Promise<boolean> = 
+  updatePost: (id: number, newTitle: string, newDescription: string, verifyCreator: User) => Promise<boolean> =
     async (id: number, newTitle: string, newDescription: string, verifyCreator: User) => {
-      if(this.posts[id].id !== id || this.posts[id].creator.id !== verifyCreator.id){
+      if (this.posts[id].id !== id || this.posts[id].creator.id !== verifyCreator.id) {
         return false;
       }
-      if(newDescription){
+      if (newDescription) {
         this.posts[id].description = newDescription;
       }
-      if(newTitle){
+      if (newTitle) {
         this.posts[id].title = newTitle;
       }
       this.posts[id].modifiedAt = new Date();
       return true;
     }
 
-    getPost : (id: number) => Promise<Post> = 
-      async (id: number) => {
-        if(await this.findById(id)){
-          return this.posts[id];
-        }
-        throw Error("Post not found");
+  getPost: (id: number) => Promise<Post> =
+    async (id: number) => {
+      if (await this.findById(id)) {
+        return this.posts[id];
+      }
+      throw Error("Post not found");
     }
 
   // Returns true if post is deleted given id and user id
@@ -114,6 +116,6 @@ export class PostService implements IPostService{
 }
 
 // Returns empty Postservice
-export function makePostService() : PostService{
+export function makePostService(): PostService {
   return new PostService({});
 }
