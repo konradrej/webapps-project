f6dcee6967c1d10dd3098e2160b8d7f314ae41e7
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2022-02-14 13:01:27 +0100

    Added CardDetail popup, updated styling and added test for GridItem

--- client/src/Components/GridItem/GridItem.tsx
+++ client/src/Components/GridItem/GridItem.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import { Card } from "react-bootstrap";
+import CardDetails from "../CardDetails";
 import styles from "./GridItem.module.css";
 
-type Props = {
+export type Props = {
   user: {
     username: string,
-    imageUrl: string
+    profileImageUrl: string
   },
   post: {
     title: string,
@@ -17,22 +18,48 @@ type Props = {
 }
 
 export default class GridItem extends React.Component<Props>{
-  render(){
+  state = {
+    showCardDetails: false
+  }
+
+  onClickHandler = () : void => {
+    this.setState({showCardDetails: true});
+  }
+
+  onClose = () : void => {
+    this.setState({showCardDetails: false});
+  }
+
+  render() : JSX.Element {
     return (
-      <Card className={styles.card}>
-        <Card.Img variant="top" src={this.props.post.imageUrl} alt={this.props.post.imageAlt} />
-        <Card.Body>
-          <Card.Title>{this.props.post.title}</Card.Title>
-          <Card.Subtitle className={styles["card-subtitle"]}>
-            <img src={this.props.user.imageUrl} alt="" />
-            <span className={styles['text-muted']}>{this.props.user.username}</span>
-          </Card.Subtitle>
-          <Card.Text className="text-end">
-            <small className={styles['text-muted']}>{this.props.post.created}</small>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <>
+        <Card className={styles.card} onClick={ this.onClickHandler }>
+          <Card.Img variant="top" src={this.props.post.imageUrl} alt={this.props.post.imageAlt} />
+          <Card.Body>
+            <Card.Title>{this.props.post.title}</Card.Title>
+            <Card.Subtitle className={styles["card-subtitle"]}>
+              <img src={this.props.user.profileImageUrl} alt="" />
+              <span className={styles['text-muted']}>{this.props.user.username}</span>
+            </Card.Subtitle>
+            <Card.Text className="text-end">
+              <small className={styles['text-muted']}>{this.props.post.created}</small>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        {
+          this.state.showCardDetails ? 
+            <CardDetails
+              postImageURL={this.props.post.imageUrl}
+              postTitle={this.props.post.title}
+              postDate={this.props.post.created}
+              postDescription={this.props.post.description}
+              userImage={this.props.user.profileImageUrl}
+              userName={this.props.user.username}
+              onClose={ this.onClose }
+            />
+            : null
+        }
+      </>
     )
   }
 }
-
