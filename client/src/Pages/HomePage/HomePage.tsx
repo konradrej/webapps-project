7b81f382b32947417a47ddeef721ae5edc40fa88
Erik Kieu <erik.kieu@gmail.com>
2022-02-14 13:17:12 +0100

    stash

--- client/src/Pages/HomePage/HomePage.tsx
+++ client/src/Pages/HomePage/HomePage.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Header from "../../Components/Header";
 
 interface HomePageProps {
 
 }
 
-export default function HomePage(props : HomePageProps) {
+export default function HomePage(props: HomePageProps) {
   return (
-    <div>Home page :D</div>
+    <><Header></Header><div>Home page :D</div></>
   );
 }
