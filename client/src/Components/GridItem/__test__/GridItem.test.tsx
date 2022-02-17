14c2d1d1fb8f93a24a2620d27a1fa6432d10686f
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2022-02-17 13:38:14 +0100

    Move GridItem test to __test__ subfolder

--- client/src/Components/GridItem/__test__/GridItem.test.tsx
+++ client/src/Components/GridItem/__test__/GridItem.test.tsx
@@ -1,6 +1,6 @@
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import GridItem from "./GridItem";
+import GridItem from "../GridItem";
 
 let container : HTMLElement | null = null;
 
