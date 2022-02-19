f46b986802a6760bd8eb5c4d0c404a1c1da4f363
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2022-02-19 16:40:30 +0100

    Changed item rendering, removed SortSelector in ItemGrid..
    ItemGrid:
    - Changed item rendering from creating new GridItems every render to generating GridItems on create and prop update and storing the GridItems in the component state. This resolves popup dissappering on resize as the GridItems do not get remade and lose existing state.
    - Removed SortSelector.

--- client/src/Components/ItemGrid/ItemGrid.tsx
+++ client/src/Components/ItemGrid/ItemGrid.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import GridItem, { Props as GridItemProps } from "../GridItem/GridItem";
-import SortSelector from "../SortSelector/SortSelector";
 import Masonry from "react-masonry-css";
 import styles from "./ItemGrid.module.css";
 
 export type Props = {
   posts : GridItemProps[],
   breakpointColumns? : { default : number, [key : number] : number }
+}
+
+type State = {
+  items: ReactElement[]
 }
 
 const breakpointColumns = {
@@ -18,22 +21,45 @@ const breakpointColumns = {
 }
 
 export default class ItemGrid extends React.Component<Props>{
+  state: State = {
+    items: []
+  }
+
+  constructor(props: Props) {
+    super(props);
+
+    this.createItems();
+  }
+
+  createItems = (): void => {
+    const items: JSX.Element[] = [];
+
+    this.props.posts.map((post: any, i: number) => {
+      items.push(<GridItem key={i} {...post} />);
+    })
+
+    this.setState({items: items});
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if(this.props.posts != prevProps.posts){
+      this.createItems();
+    }
+  }
+
   render() : JSX.Element {
     return (
-      <>
-        <SortSelector className={styles.selector + " text-end "} />
-        <Masonry
-          breakpointCols={this.props.breakpointColumns ?? breakpointColumns}
-          className={styles["grid"]}
-          columnClassName={styles["grid-column"]}
-        >
-          {
-            this.props.posts.map((post, i) => {
-              return <GridItem key={i} {...post} />;
-            })
-          }   
-        </Masonry>
-      </>
+      <Masonry
+        breakpointCols={this.props.breakpointColumns ?? breakpointColumns}
+        className={styles["grid"]}
+        columnClassName={styles["grid-column"]}
+      >
+        {
+          this.state.items.map((item: any, _: number) => {
+            return item;
+          })
+        }   
+      </Masonry>
     )
   }
 }
