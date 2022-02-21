d84a64b96e2a585a731aa3bf59b46eea7fb91269
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2022-02-21 11:29:32 +0100

    Create homepage, adjust ItemGrid breakpoints, add ItemGrid no items message.

--- client/src/Pages/HomePage/HomePage.tsx
+++ client/src/Pages/HomePage/HomePage.tsx
@@ -1,14 +1,78 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import ItemGrid from "../../Components/ItemGrid/ItemGrid";
+import SortSelector from "../../Components/SortSelector/SortSelector";
+import { Props as GridItemProps } from "../../Components/GridItem/GridItem";
 import Header from "../../Components/Header";
+import axios from "axios";
+import styles from "./HomePage.module.css";
+import PopUp from "../../Components/Pop-ups/Pop-up";
 
-interface HomePageProps {
-
+export type Props = {
+  
 }
 
-export default function HomePage(props: HomePageProps) {
-  return (
-    <Header>
+type State = {
+  posts: GridItemProps[],
+  errorPopup?: JSX.Element
+}
 
-    </Header>
-  )
+export default class HomePage extends React.Component<Props>{
+  state: State = {
+    posts: [],
+    errorPopup: undefined
+  }
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  getPosts = (order?: string): void => {
+    let url: string = "http://localhost:8080/post/";
+
+    if(order){
+      url += "?order=" + order;
+    }
+
+    axios.get(url, {timeout: 1000}).then((res) => {
+      res.data.map((value : any, _ : number) => {
+        value.createdAt = new Date(value.createdAt);
+      });
+
+      this.setState({
+        posts: res.data
+      });
+    }).catch((error) => {
+      this.setState({
+        errorPopup: <PopUp onClose={this.onClose}><span>Unable to get posts.</span><br /><span>Please try again later.</span></PopUp>
+      });
+    })
+  }
+
+  onClose = (): void => {
+    this.setState({
+      errorPopup: undefined
+    });
+  }
+
+  onSelect = (eventKey: string): void => {
+    this.getPosts(eventKey);
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+ 
+  render() {
+    return (
+      <>
+        <Header />
+        <Container>
+          <SortSelector className={styles.selector + " text-end "} onSelect={this.onSelect}/>
+          <ItemGrid posts={this.state.posts} />
+        </Container>
+        {this.state.errorPopup ?? null}
+      </>
+    )
+  }
 }
