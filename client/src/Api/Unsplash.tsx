b871315e51665de455f5ae48738122ae8c3c3302
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2022-02-27 13:07:23 +0100

    Add Unsplash API

---
+++ client/src/Api/Unsplash.tsx
@@ -0,0 +1,42 @@
+import axios, { AxiosResponse } from "axios";
+import styles from "./Unsplash.module.css";
+
+type UnsplashItem = {
+  id: string,
+  thumbnailUrl: string,
+  imageUrl: string,
+  imageAlt: string,
+  pageUrl: string
+}
+
+export const searchUnsplash = async function (search: string): Promise<any> {
+  return axios.get("https://api.unsplash.com/search/photos/", {
+    params: {
+      query: search,
+      client_id: "CLIENT_ACCESS_KEY",
+      per_page: 25
+    }
+  }).then((res: AxiosResponse): UnsplashItem[] => {
+    return res.data.results.map((value: any, _: number) => {
+      return {
+        id: value.id,
+        thumbnailUrl: value.urls.small,
+        imageUrl: value.urls.full,
+        imageAlt: value.alt_description,
+        pageUrl: value.links.html
+      }
+    });
+  }).then((results: UnsplashItem[]): JSX.Element[] => {
+    return createItems(results);
+  })
+}
+
+const createItems = (results: UnsplashItem[]): JSX.Element[] => {
+  return results.map((value: any, i: number) => {
+    return (
+      <a key={i} href={value.pageUrl} className={styles.unsplash_item} target="_blank" rel="noreferrer">
+        <img src={value.thumbnailUrl} alt={value.imageAlt} />
+      </a>
+    )
+  });
+}
