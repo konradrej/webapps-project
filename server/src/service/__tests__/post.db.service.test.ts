8cca8ede62fa20f9de9f8cddc45571080b37db50
applephong <apple_phong@hotmail.com>
2022-03-11 22:08:53 +0100

    Update postDBservice test

--- server/src/service/__tests__/post.db.service.test.ts
+++ server/src/service/__tests__/post.db.service.test.ts
@@ -66,49 +66,4 @@ test("Setup a inmemory database and creating a post and updating the post with n
   })
 });
 
-test("Setup a inmemory database and creating a post and try to update with invalid id and creator", async () => {
-  const mongod = await MongoMemoryServer.create();
-  const uri = mongod.getUri();
-  const testConn = createConnection(uri)
-  const testModel = testConn.model<Post>("Post", PostSchema)
-  const postService = new PostDBService(testModel);
-  return await postService.createPost("postTitle", "postDescription", "postURL", 0).then( async (post : Post) => {
-    expect(postService.updatePost( post.id, "newPostTitle", "postDescription" , 1)).rejects.toThrowError(new Error("Specified user is not creator"));
-    expect(postService.updatePost(3, "newTitle", "newDescription", 0)).rejects.toThrowError(new Error("Post not found"))
-  })
-});
 
-test("Setup a inmemory database and creating 4 posts and check the order of the posts", async () => {
-  const mongod = await MongoMemoryServer.create();
-  const uri = mongod.getUri();
-  const testConn = createConnection(uri)
-  const testModel = testConn.model<Post>("Post", PostSchema)
-  const postService = new PostDBService(testModel);
-  await postService.createPost("dPostTitle", "postDescription", "postURL", 0);
-  await postService.createPost("cPostTitle", "postDescription", "postURL", 0);
-  await postService.createPost("aPostTitle", "postDescription", "postURL", 0);
-  await postService.createPost("bPostTitle", "postDescription", "postURL", 0);
-
-  return await postService.getPosts("title-ascending").then(async (alphabetic : Post []) => {
-    expect(alphabetic[0].title).toBe("aPostTitle");
-    expect(alphabetic[1].title).toBe("bPostTitle");
-    expect(alphabetic[2].title).toBe("cPostTitle");
-    expect(alphabetic[3].title).toBe("dPostTitle");
-
-    await postService.getPosts("title-descending").then(async (reverse : Post []) => {
-      expect(reverse[0].title).toBe("dPostTitle");
-      expect(reverse[1].title).toBe("cPostTitle");
-      expect(reverse[2].title).toBe("bPostTitle");
-      expect(reverse[3].title).toBe("aPostTitle");
-      
-      // getPosts("") is default
-      await postService.getPosts("recent-descending").then(async (recency : Post []) => {
-        /*Should be the reverse order as they were created b>a>c>d*/
-        expect(recency[0].title).toBe("bPostTitle");
-        expect(recency[1].title).toBe("aPostTitle");
-        expect(recency[2].title).toBe("cPostTitle");
-        expect(recency[3].title).toBe("dPostTitle");
-      })
-    })
-  })
-});
